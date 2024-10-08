6fa3532e8b6ecab8050b1051eb2d7c932fa97f9c
manjaro_luo <luo1531407383@gmail.com>
2024-10-08 13:07:10 +0800

    manjaro-hello

---
+++ fish/config.fish
@@ -0,0 +1,11 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+alias _pcs='sudo proxychains'
+alias _lyf='proxychains sudo pacman -S '
+# alias _telegram='/home/a/snap/_telegram'
+# alias _bjz='sudo pacman -S '
+alias _start_docker='sudo systemctl start docker.service'
+alias _uc='bash /home/a/.config/clash/update_subscription.sh'
+alias _up='sudo pacman -Syu'
+alias _clr='clear'
